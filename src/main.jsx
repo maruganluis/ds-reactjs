c7e021acc2d8ed58fed0fdbdd7921289150e0597
Luis MB <maruganluis@gmail.com>
2025-01-25 14:25:57 +0100

    feat(ds): add deisgn system tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './ui-kit/tokens/primitives.css';
+import './ui-kit/tokens/my-company-brand.css';
+import './index.css';
+import App from './App.jsx';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
